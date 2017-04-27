ee5ad91b2043a6d0d73dea3c1609a9c1f1b51e79
Fayez Nassar <fayeznas@gmail.com>
2017-04-27 23:09:32 +0300

    B+ tree, insert implementation.

--- BEpsilon.h
+++ BEpsilon.h
@@ -1,46 +1,277 @@
 #ifndef BEPSILON_BEPSILON_H
 #define BEPSILON_BEPSILON_H
 
+#include <iostream>
 #include <vector>
 
 using namespace std;
-template <typename Key, typename Value, int Degree>
-class BEpsilon {
+
+template <typename Key, typename Value, int B>
+class BEpsilonTree {
+public:
+
+    void insert(Key key, Value value);
+
+    Value pointQuery(Key key);
+
+    vector<Value> rangeQuery(Key minKey, Key maxKey);
+
+    void remove(Key key);
 
 private:
 
     class Node {
+    public:
+
+        Node(bool leaf, Node* parent = NULL, Node* sibling = NULL);
+
+        //A utility constructor for copy the node content from other to this,
+        //it's useful for split child, to avoid losing the parent child pointer.
+        Node(Node& other);
+
+        //A function to check if the node(leaf/internal is full orr not).
+        bool isFull();
+
+        // A function to search a key in subtree rooted with this node.
+        // returns NULL if k is not present
+        Node *search(Key key);
+
+
+        // A function that returns the index of the first key that is greater
+        // or equal to k
+        int findKey(Key key);
+
+        //A function to find the index of this node in the his parent children vector
+        int findChild();
+
+
+        // A utility function to split the child of this node. ix is index
+        // of child in child vector. The Child must be full when this
+        // function is called
+        void splitChild(int ix, Node *child);
+
+
+        // A utility function to insert a new key in the subtree rooted with
+        // this node. The assumption is, the node must be non-full when this
+        // function is called
+        void insert(Key key, Value value);
+
+        //A utility function to make sure that all the leaf is on the same height.
+        //should call after the key-value insertion from the leaf.
+        void insertKeysUpdate();
+
     private:
         bool leaf;
-
-        //if leaf == true
-        Key key;
-        Value value;
-
-        //else
-        //rep invariant: (Degree/2) <= keys.size() < (Degree)
+        Node* parent;
         vector<Key> keys;
-        vector<Node*> childern;
+
+        //if this node is a leaf
+        //values.size() == keys.size(),for now it's equal to 1, keys.size.max == B-1;
+        vector<Value> values;
+        Node* sibling;
+
+        //if the node is internal
+        //children.size() == keys.size()+1;
+        vector<Node*> children;
+
+        friend class BEpsilonTree;
     };
 
     Node* root;
+};
 
-public:
+template <typename Key, typename Value, int B>
+BEpsilonTree<Key, Value, B>::Node::Node(bool leaf, Node* parent, Node* sibling) {
+    this->parent = parent;
+    this->sibling = sibling;
+    this->leaf = leaf;
+};
 
-    //update if Key exist;
-    void insert(Key key, Value value) {
-    }
+template <typename Key, typename Value, int B>
+BEpsilonTree<Key, Value, B>::Node::Node(Node& other) {
 
-    Value pointQuery(Key key) {
-        return Value();
-    }
+    //copy/cut the content from other to this
+    this->leaf = other.leaf;
+    this->parent = other.parent;
 
-    vector<Value> rangeQuery(Key minKey, Key maxKey) {
-        return vector<Value>();
-    }
+    this->keys.insert(this->keys.begin(), other.keys.begin(), other.keys.end());
+    other.keys.erase(other.keys.begin(), other.keys.end());
 
-    void remove(Key key) {
+    this->children.insert(this->children.begin(), other.children.begin(), other.children.end());
+    other.children.erase(other.children.begin(), other.children.end());
+
+    this->values.insert(this->values.begin(), other.values.begin(), other.values.end());
+    other.values.erase(other.values.begin(), other.values.end());
+}
+
+template <typename Key, typename Value, int B>
+bool BEpsilonTree<Key, Value, B>::Node::isFull() {
+    //choose the max number of key and values in each node according to the block size.
+    return this->keys.size()==B;
+}
+
+template <typename Key, typename Value, int B>
+int BEpsilonTree<Key, Value, B>::Node::findChild() {
+    int ix = 0;
+    //for sure this node isn't root and full, we check it before this function call.
+    typedef typename vector<BEpsilonTree<Key, Value, B>::Node*>::iterator iterator;
+    for(iterator it = this->parent->children.begin(); it != this->parent->children.end(); it++) {
+        if ((*it) == this) {
+            return ix;
+        } else {
+            ix++;
+        }
     }
 };
+
+template <typename Key, typename Value, int B>
+void BEpsilonTree<Key, Value, B>::Node::splitChild(int ix, BEpsilonTree<Key, Value, B>::Node *left_child) {
+
+    //this node is the parent of right and left child.
+    // Create a new node which is going to store (child->keys.size()-1) keys of child
+    BEpsilonTree<Key, Value, B>::Node *right_child = new BEpsilonTree<Key, Value, B>::Node(left_child->leaf,
+                                                                                           left_child->parent,
+                                                                                           left_child->sibling);
+
+    //update the sibling of both child and new node, add the new node between the child and the new node.
+    //if the nods is internal and not a leaf, the sibling will be NULL.
+    right_child->sibling = left_child->sibling;
+    left_child->sibling = left_child->leaf ? right_child : NULL;
+
+    //update to move the minimum number of children for each node, and not 1.
+    //B should be grater than 2, else infinite loop will occur.
+    int middle_ix = B/2;
+
+
+    if(left_child->leaf) {
+        right_child->keys.insert(right_child->keys.begin(),
+                                 left_child->keys.begin()+middle_ix,
+                                 left_child->keys.end());
+
+        left_child->keys.erase(left_child->keys.begin()+middle_ix,
+                               left_child->keys.end());
+
+        right_child->values.insert(right_child->values.begin(),
+                                   left_child->values.begin()+middle_ix,
+                                   left_child->values.end());
+
+        left_child->values.erase(left_child->values.begin()+middle_ix,
+                                 left_child->values.end());
+
+        this->keys.insert(this->keys.begin()+ix, right_child->keys[0]);
+    } else {
+
+        //find the appropriate key and its index.
+        int key = left_child->keys[middle_ix];
+        int key_ix = 0;
+        while((key_ix >= 0) && (key_ix < this->keys.size()) && (this->keys[key_ix] < key)) {
+            key_ix++;
+        }
+
+        this->keys.insert(this->keys.begin()+key_ix, key);
+
+        right_child->keys.insert(right_child->keys.begin(),
+                                 left_child->keys.begin()+(middle_ix+1),
+                                 left_child->keys.end());
+
+        left_child->keys.erase(left_child->keys.begin()+(middle_ix),
+                               left_child->keys.end());
+
+        right_child->children.insert(right_child->children.begin(),
+                                     left_child->children.begin()+middle_ix+1,
+                                     left_child->children.end());
+
+        // w/o this, compilation error, with nested template in dependent scope.
+        typedef typename vector<BEpsilonTree<Key, Value, B>::Node*>::iterator iterator;
+        for(iterator it = left_child->children.begin()+middle_ix+1; it != left_child->children.end(); it++) {
+            (*it)->parent = right_child;
+        }
+
+        left_child->children.erase(left_child->children.begin()+middle_ix+1,
+                                   left_child->children.end());
+
+
+    }
+
+    //set the new node as a child of this node
+    this->children.insert(this->children.begin()+(ix+1), right_child);
+
+};
+
+template <typename Key, typename Value, int B>
+void BEpsilonTree<Key, Value, B>::Node::insertKeysUpdate() {
+    if(this->isFull()) {
+        if(this->parent == NULL) {//this is root :)
+            Node *node = new Node(false);
+            //move it the the end;
+            this->parent = node;
+            node->children.insert(node->children.begin(), this);
+            node->splitChild(0, this);
+        } else {
+            this->parent->splitChild(this->findChild(), this);
+            this->parent->insertKeysUpdate();
+        }
+    }
+};
+
+template <typename Key, typename Value, int B>
+void BEpsilonTree<Key, Value, B>::Node::insert(Key key, Value value) {
+
+    //for finding the key position, we'll start with last index
+    int ix = this->keys.size()-1;
+    //find the key index.
+    while(ix >= 0 && keys[ix] > key) {
+        ix--;
+    }
+
+    if(this->leaf) {
+        this->keys.insert(this->keys.begin()+(ix+1), key);
+        this->values.insert(this->values.begin()+(ix+1), value);
+        this->insertKeysUpdate();
+    } else {//this is internal node
+        ix = ix == -1 ? 0 : ix;
+        if(this->keys[ix] < key) {
+            ix++;
+        }
+        this->children[ix]->insert(key, value);
+    }
+};
+
+/*
+ * the API B+ function
+ * insert: A function for insertion to the tree
+ * rangeQuery:
+ * Query:
+ * remove: A function to remove a key from the tree.s
+ * */
+
+template <typename Key, typename Value, int B>
+void BEpsilonTree<Key, Value, B>::insert(Key key, Value value) {
+    if(root == NULL) { // if the Tree is empty
+        root = new BEpsilonTree<Key, Value, B>::Node(true);
+        root->keys.insert(root->keys.begin(), key);
+        root->values.insert(root->values.begin(), value);
+    } else { //if the root is not null.
+        root->insert(key, value);
+        if(root->parent != NULL) {
+            root = root->parent;
+        }
+    }
+};
+
+template <typename Key, typename Value, int B>
+vector<Value> BEpsilonTree<Key, Value, B>::rangeQuery(Key minKey, Key maxKey) {
+
+}
+
+template <typename Key, typename Value, int B>
+Value BEpsilonTree<Key, Value, B>::pointQuery(Key key) {
+
+};
+
+template <typename Key, typename Value, int B>
+void BEpsilonTree<Key, Value, B>::remove(Key key) {
+    return;
+}
 
 #endif //BEPSILON_BEPSILON_H
